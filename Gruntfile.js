d40637433d98ac6ccbee373479c9d3a84b8e57d7
jsanahuja <bannss1@gmail.com>
2020-01-26 15:42:05 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        babel: {
+            options: {
+                sourceMap: false,
+                presets: ['@babel/preset-env', 'minify']
+            },
+            dist: {
+                files: {
+                    'dist/Notifier.min.js': 'src/Notifier.js'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/Notifier.min.css': ['src/*.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('dist', ['babel', 'cssmin']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Notifier.js
-Vanilla Javascript notification dispatcher
+Vanilla Javascript html notifications manager
+
+[Full documentation and examples here](https://www.sowecms.com/demos/Notifier.js)
